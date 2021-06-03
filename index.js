7ea14016b8d239777c8fbd7cb8ef21b3e8634b80
AD9018 <adamdadi23@gmail.com>
2021-06-03 13:59:28 -0400

    WIP

--- index.js
+++ index.js
@@ -1,13 +1,30 @@
 require("dotenv").config();
-const express = require("express");
+
 const server = express();
+const cors = require("cors")
+const path = require("path")
+const express = require("express");
+
 console.log(process.env.USER); // env USER=gabriel
 console.log(process.env.SHELL); // env SHELL=/bin/zhs
+
+server.use(express.json())
+server.use(cors());
+server.use(express.static(path.join(__dirname,'client/build')))
+
+
+
 if (process.env.NODE_ENV === "production") {
   console.log("this means this code is deployed");
 }
+
 const PORT = process.env.PORT || 5000;
 console.log("port is -> ", PORT);
+
+
+server.get("/",  (req, res)=>{
+    res.sendFile(path.join(__dirname,'client/build', 'index.html'))
+})
 
 server.get("/api", (req, res) => {
   res.json({ message: `${process.env.COHORT} ROCKS` });
